f386a636df351e0fba3a192f34a2ee39952d5d0d
akhivyk <akhivuk.laba@solvd.com>
2022-12-06 11:21:54 +0300

    add

---
+++ src/com/solvd/transfercompany/order/OrderException.java
@@ -0,0 +1,7 @@
+package com.solvd.transfercompany.order;
+
+public class OrderException extends  Exception {
+    public OrderException(String message) {
+        super(message);
+    }
+}
---
+++ src/com/solvd/transfercompany/transport/TransportException.java
@@ -0,0 +1,7 @@
+package com.solvd.transfercompany.transport;
+
+public class TransportException extends Exception {
+    public TransportException (String message) {
+        super(message);
+    }
+}
